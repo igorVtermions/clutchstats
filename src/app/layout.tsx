9898e8b88b90b2f027b230286f2ba6b0229be618
Igor vinicius <igorviniciusf10@gmail.com>
2024-12-18 16:22:15 -0300

    feat: Criando Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import Header from "@/components/Header";
 import type { Metadata } from "next";
+import './globals.css';
 
 export const metadata: Metadata = {
   title: "Clucth Stats",
@@ -12,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body><Header />
+      {children}
+      </body>
     </html>
   );
 }
